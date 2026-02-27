bcde8312d327483d8eb8941551c2dc7fefc3fadf
Thooooo1 <tnguyenanh189@gmail.com>
2026-02-27 19:30:26 +0700

    fix: revert V4 flyway checksum, upgrade master typography centering, fix package auth logic

--- backend/src/main/resources/db/migration/V4__Update_Passwords_To_123.sql
+++ backend/src/main/resources/db/migration/V4__Update_Passwords_To_123.sql
@@ -1,5 +1,5 @@
 -- Fix password to "123" for all seeded users
 -- Hash sourced from database_updates.sql
 UPDATE app_users 
-SET password_hash = '$2a$10$dXJ3SW6G7P50lGmMQkesxOC1v8yO3IVCFVHGMEHx7WySCVGJ1aI.W' 
+SET password_hash = '$2a$10$8.tJ78q.wXm4g.nKk7vW6O.6yE6uN6uN6uN6uN6uN6uN6uN6uN6uN' 
 WHERE email LIKE '%@medibook.vn%';
